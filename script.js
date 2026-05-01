b487a6ecb7ed7da1c259ff59265ac1e86fc61fe9
Achraf Soukkane <achrafsoukkane@gmail.com>
2026-05-01 17:50:32 +0100

    final version mini projet web

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function message(){
-    alert("Message envoyé");
+function message() {
+  let nom = document.getElementById("nom").value;
+  let email = document.getElementById("email")?.value;
+
+  if (nom === "") {
+    alert("Veuillez entrer votre nom");
+    return;
+  }
+
+  if (email !== undefined && email === "") {
+    alert("Veuillez entrer votre email");
+    return;
+  }
+
+  alert("Message envoyé avec succès ✅");
 }
